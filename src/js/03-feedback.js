1de83e2d19eb82dcdbfff4168d7b7c56f254efee
Yuriichuk-Olha <olga.kantemyr@gmail.com>
2023-01-19 01:27:36 +0100

    Update 03-feedback.js
    change 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,11 +7,12 @@ const form = document.querySelector('.feedback-form');
 
 const STOREGE_KEY = 'feedback-form-state';
 
+onMessageText();
+
 button.addEventListener('submit', onFormSubmit)
 function onFormSubmit(event) {
         console.log(event);
             event.preventDefault();
-    
             event.currentTarget.reset()
             localStorage.removeItem(STOREGE_KEY)
     }
@@ -19,22 +20,25 @@ function onFormSubmit(event) {
 form.addEventListener('input', throttle(function(){
 
     const data = {
-        input:form.email.value,
+    input:form.email.value,
     textarea: form.message.value,
 }
-//console.log(data);
     localStorage.setItem(STOREGE_KEY, JSON.stringify(data));
-
-            const saveMessage = localStorage.getItem(STOREGE_KEY);
-        console.log(saveMessage);
-    if(saveMessage){
-        const parseMessage = JSON.parse(saveMessage);
-//console.log(saveMessage);
-    }
 }, 500)
 )
 
+function onMessageText() {
+    const saveMessage = localStorage.getItem(STOREGE_KEY)
+    if(saveMessage){
+        const parseMessage = JSON.parse(saveMessage)
 
+        form.email.value = parseMessage.input.value;
+        console.log(form.email.value);
+        form.message.value = parseMessage.textarea.value;
+        console.log(parseMessage);
+        return
+    }
+}
 
 
 
